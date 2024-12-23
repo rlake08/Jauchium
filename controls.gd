b3384d2a8be2d87c40ec8975fb54378c44f89127
SpikeTrapBoomStudios <spiketrapboomstudios@gmail.com>
2024-12-23 00:37:37 -0500

    big electron physics update + helpful functions for all particles

--- controls.gd
+++ controls.gd
@@ -7,6 +7,15 @@ var MouseLFunction: = "Empty";
 
 @export var Speed = CameraSpeed;
 
+func _ready() -> void:
+	var size_x = ProjectSettings.get_setting("display/window/size/viewport_width")*(1/camera_2d.zoom.x)
+	var size_y = ProjectSettings.get_setting("display/window/size/viewport_height")*(1/camera_2d.zoom.y)
+	var size = Vector2(size_x,size_y)
+	WindowSpace.left_edge = global_position.x-size_x/2
+	WindowSpace.right_edge = global_position.x+size_x/2
+	WindowSpace.top_edge = global_position.y-size_y/2
+	WindowSpace.bottom_edge = global_position.y+size_y/2
+
 func MouseExec(Function):
 	print (Function); #Placeholder Function
 #Add elif statement for creating particles that links to an external script
